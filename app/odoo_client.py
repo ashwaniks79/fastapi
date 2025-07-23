787fb9870fbf549d89d29182336e8d0d8908d122
fiverbaseline <fiverrbaseline@gmail.com>
2025-07-23 13:47:18 +0530

    updated code

--- app/odoo_client.py
+++ app/odoo_client.py
@@ -1,16 +1,31 @@
-app/odoo_client.py
+# app/odoo_client.py
+
+# import odoorpc
+# import os
+
+# def get_odoo_connection():
+#     odoo = odoorpc.ODOO(
+#         host=os.getenv("ODOO_HOST", "odoo-odoo-1"),
+#         port=int(os.getenv("ODOO_PORT", 8069))
+#     )
+#     odoo.login(
+#         db=os.getenv("ODOO_DB", "eredoxproapi1"),
+#         login=os.getenv("ODOO_USER", "odoo"),
+#         password=os.getenv("ODOO_PASSWORD", "odoo")
+#     )
+#     return odoo
 
 import odoorpc
 import os
 
 def get_odoo_connection():
     odoo = odoorpc.ODOO(
-        host=os.getenv("ODOO_HOST", "odoo-odoo-1"),
+        host=os.getenv("ODOO_HOST", "170.64.163.105"),       
         port=int(os.getenv("ODOO_PORT", 8069))
     )
     odoo.login(
-        db=os.getenv("ODOO_DB", "eredoxproapi1"),
-        login=os.getenv("ODOO_USER", "odoo"),
-        password=os.getenv("ODOO_PASSWORD", "odoo")
+        db=os.getenv("ODOO_DB", "odoo"),                       
+        login=os.getenv("ODOO_USER", "test@baselineitdevelopment.com"), 
+        password=os.getenv("ODOO_PASSWORD", "Base#")          
     )
     return odoo
