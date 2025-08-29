991fb3e49a54e705abf77ae889283e5a86c6b290
fiverbaseline <fiverrbaseline@gmail.com>
2025-08-29 19:45:34 +0530

    bug fixes

--- alembic/env.py
+++ alembic/env.py
@@ -1,15 +1,13 @@
 from logging.config import fileConfig
 
-from logging.config import fileConfig
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
 from alembic import context
+from app.models import Base
 
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app.database import Base
-from app.models import User, CompanyInformationPageDetails, Subscription
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -23,9 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-# target_metadata = None
 target_metadata = Base.metadata
-
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
